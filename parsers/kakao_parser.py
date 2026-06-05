fc8ae03dbfb9991f21bbff53e108d4460cb95a5c
happydain <60496888+happydain@users.noreply.github.com>
2026-06-06 01:59:12 +0900

    Update kakao_parser.py

--- parsers/kakao_parser.py
+++ parsers/kakao_parser.py
@@ -7,9 +7,5 @@ from parsers.parser_utils import (
     detect_subject,
     parse_dates_from_text,
     parse_time_range,
-    make_row,
+    make_row
 )
-
-DEFAULT_HOURLY_FEE = 100000
-DEFAULT_LOCATION = "줌"
-DEFAULT_INDUSTRY = "기타업"
