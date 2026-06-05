4b5a4941fd1d5dbf07e595bf1a974c02b9455873
happydain <60496888+happydain@users.noreply.github.com>
2026-06-06 03:02:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ if menu == "📥 보건스케줄 입력":
                         df_excel = parse_jungdae_excel(uploaded_file, common_requester,
                                                         request_date_str, year)
                     else:
-                        df_excel = parse_suwon_excel(uploaded_file, common_agency, "")
+                        df_excel = parse_suwon_excel(uploaded_file, common_agency)
                         df_excel["의뢰인"] = common_requester
                         df_excel["의뢰일"] = request_date_str
                         df_excel["의뢰방법"] = common_method
