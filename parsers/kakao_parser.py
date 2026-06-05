3540e4b9e13726e6b6d62d6229d419ca72b9ce21
happydain <60496888+happydain@users.noreply.github.com>
2026-06-06 01:47:34 +0900

    Update kakao_parser.py

--- parsers/kakao_parser.py
+++ parsers/kakao_parser.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+from parser_utils import (
+    detect_subject,
+    detect_target,
+    detect_agency,
+    parse_dates_from_text,
+    parse_time_range,
+    extract_instructor,
+    make_row
+)
