cbb8bd83e4d41c713dd90167de115420737d6925
happydain <60496888+happydain@users.noreply.github.com>
2026-06-06 02:39:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ if menu == "📥 보건스케줄 입력":
                         df_excel = parse_suwon_excel(uploaded_file, common_agency, "")
                         df_excel["의뢰인"] = common_requester
                         df_excel["의뢰일"] = request_date_str
-                        df_excel["의뢰방법"] = common_method
+                        df_excel["의뢰방법"] = common_method
 
                     if df_excel.empty:
                         st.warning("변환된 일정이 없습니다.")
