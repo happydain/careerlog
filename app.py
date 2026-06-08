f8e4d2ea0b25eb70308de3301a6cf9032b7807de
happydain <60496888+happydain@users.noreply.github.com>
2026-06-08 09:34:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ if menu == "📥 보건스케줄 입력":
         else:
             common_requester = selected_requester
     with col4:
-        common_method = st.selectbox("의뢰방법", ["카카오톡", "이메일", "전화", "문자", "기타"])
+        common_method = st.selectbox("의뢰방법", ["카카오톡엑셀", "카카오톡문자", "이메일", "전화", "문자", "기타"])
 
     request_date_str = common_date.strftime("%Y-%m-%d")
 
