de0130f6e58bd42c0039758dc14c35f7401fec43
happydain <60496888+happydain@users.noreply.github.com>
2026-06-09 23:49:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ if menu == "🏠 대시보드":
 
     custom_css = """
         .fc-event-title {
-            font-size: 8px !important;
+            font-size: 9px !important;
             white-space: nowrap;
             overflow: hidden;
             text-overflow: ellipsis;
