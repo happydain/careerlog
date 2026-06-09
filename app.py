09913e9e62ed73c362a8097171ab9d8f4e7942b4
happydain <60496888+happydain@users.noreply.github.com>
2026-06-09 22:31:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from utils import calc_hours, calc_fee, get_column_config
 
 from gsheet import (
     append_to_gsheet, load_gsheet_raw, load_gsheet_final,
-    save_gsheet_final, append_evidence_to_sheet, replace
+    save_gsheet_final, append_evidence_to_sheet, replace_gsheet
 )
 
 from parsers import (
