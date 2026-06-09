3a26fa95c583c8c65c108d9e213c4569a36fdb15
happydain <60496888+happydain@users.noreply.github.com>
2026-06-09 11:37:33 +0900

    Update __init__.py

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -2,5 +2,6 @@ from .kakao_general import parse_kakao_text
 from .kakao_seoul import parse_seoul_kakao
 from .kakao_hanahn import parse_hanahn_kakao
 from .excel_suwon import parse_suwon_excel
+from .excel_suwon2 import parse_suwon2_excel
 from .excel_jungdae import parse_jungdae_excel
 from .excel_incheon import parse_incheon_excel
